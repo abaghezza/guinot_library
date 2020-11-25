36b4c43832f33b19138a21a4adf0151a5bdcf58f
Abdelwahab Baghezza <wahab-38@hotmail.fr>
2020-11-25 16:39:33 +0100

    travail sur le CRUD (index, show, ) et formulaires

--- src/Form/CDRomType.php
+++ src/Form/CDRomType.php
@@ -7,18 +7,24 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 class CDRomType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('cote')
-            ->add('availability')
-            ->add('picture')
-            ->add('authtor')
-            ->add('bail')
-            ->add('loans')
+            ->add('title', TextType::class)
+            ->add('cote', TextType::class)
+            ->add('availability', CheckboxType::class)
+            ->add('picture', TextType::class)
+            ->add('authtor', TextType::class)
+            ->add('bail', NumberType::class)
+            //->add('loans', EntityType::class)
         ;
     }
 
--- src/Form/MicroFilmType.php
+++ src/Form/MicroFilmType.php
@@ -7,16 +7,26 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
 class MicroFilmType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('cote')
-            ->add('availability')
-            ->add('picture')
-            ->add('createdAt')
+            ->add('title', TextType::class)
+            ->add('cote', TextType::class)
+            ->add('availability', CheckboxType::class)
+            ->add('picture', TextType::class)
+            ->add('createdAt', DateTimeType::class, [
+                'disabled' => true,
+                'label' => 'Created (not editable)',
+            ])
             ->add('loans')
         ;
     }
